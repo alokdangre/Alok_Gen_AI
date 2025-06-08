d7fa21044bf690601a85abe98b4ec3abaaf1a425
Studycode001 <148090007+Studycode001@users.noreply.github.com>
2025-06-09 00:08:26 +0530

    some env fixes

--- hello_world/chat_gemini.py
+++ hello_world/chat_gemini.py
@@ -1,6 +1,12 @@
 from google import genai
+from dotenv import load_dotenv
+import os
 
-client = genai.Client(api_key="AIzaSyAEnchKdKmmkn6AxxkkvzkOyKl9DhDzeF0")
+load_dotenv()
+
+api_key = os.getenv("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
     model="gemini-2.0-flash",

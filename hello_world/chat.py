a75b972a7ac9fd54996ced6dc161339a430858b6
Studycode001 <148090007+Studycode001@users.noreply.github.com>
2025-06-10 00:08:54 +0530

    Add core functionality for AI assistant with vector database integration and document indexing

--- hello_world/chat.py
+++ hello_world/chat.py
@@ -38,25 +38,40 @@ SYSTEM_PROMPT = """
 # Chain of Thought Prompting: The model is encouraged to break down reasoning step by step before arriving at an answer.
 
 SYSTEM_PROMPT = """
-    You are an helpful AI assitant who is specialised in resolving user query.
-    For the given user input , analyse the input and break down the problem step by step.
-    The steps are you get a user input , you analyse, you think, you think again, and think for several times and then return the output with an explanation.
+    You are an helpfull AI assistant who is specialized in resolving user query.
+    For the given user input, analyse the input and break down the problem step by step.
+
+    The steps are you get a user input, you analyse, you think, you think again, and think for several times and then return the output with an explanation. 
+
     Follow the steps in sequence that is "analyse", "think", "output", "validate" and finally "result".
 
-    Rules: 
-    1.Follow the strict JSON output as per schema.
-    2.ALways perform one step at a time wait for the next input.
-    3.Carefully analyse the user query.
+    Rules:
+    1. Follow the strict JSON output as per schema.
+    2. Always perform one step at a time and wait for the next input.
+    3. Carefully analyse the user query,
 
-    Output Format: {{"step":"string","content":"string"}}
+    Output Format:
+    {{ "step": "string", "content": "string" }}
 
-    Example: 
+    Example:
     Input: What is 2 + 2
-    Output: {{"step":"analyse","content":"Alright! The user is interest in maths query and he is asking basic arthematic operation"}}
-    Output: {{"step":"think","content":"To perform this addition I must go from left to right and add all the operands."}}
-    Output: {{"step":"output","content":"4"}}
-    Output: {{"step":"validate","content":"Seems like 4 is correct ans for 2 +2"}}
-    Output: {{"step":"result","content":"2 +2=4 and this is calculated by adding all numbers"}}
+    Output: {{ "step": "analyse", "content": "Alight! The user is interest in maths query and he is asking a basic arthematic operation" }}
+    Output: {{ "step": "think", "content": "To perform this addition, I must go from left to right and add all the operands." }}
+    Output: {{ "step": "output", "content": "4" }}
+    Output: {{ "step": "validate", "content": "Seems like 4 is correct ans for 2 + 2" }}
+    Output: {{ "step": "result", "content": "2 + 2 = 4 and this is calculated by adding all numbers" }}
+
+    Example:
+    Input: What is 2 + 2 * 5 / 3
+    Output: {{ "step": "analyse", "content": "Alight! The user is interest in maths query and he is asking a basic arthematic operations" }}
+    Output: {{ "step": "think", "content": "To perform this addition, I must use BODMAS rule" }}
+    Output: {{ "step": "validate", "content": "Correct, using BODMAS is the right approach here" }}
+    Output: {{ "step": "think", "content": "First I need to solve division that is 5 / 3 which gives 1.66666666667" }}
+    Output: {{ "step": "validate", "content": "Correct, using BODMAS the division must be performed" }}
+    Output: {{ "step": "think", "content": "Now as I have already solved 5 / 3 now the equation looks lik 2 + 2 * 1.6666666666667" }}
+    Output: {{ "step": "validate", "content": "Yes, The new equation is absolutely correct" }}
+    Output: {{ "step": "validate", "think": "The equation now is 2 + 3.33333333333" }}
+    and so on.....
 """
 
 messages = [
@@ -77,9 +92,25 @@ while True:
     messages.append({"role": "assitant", "content": response.choices[0].message.content})
     parse_json = json.loads(response.choices[0].message.content)
 
+    ## TODO: Multi MOdel Agents
+    if parse_json.get("step") == "think":
+        # Make a Claude API Call and append the result as validate
+        messages.append({ "role": "assistant", "content": "<>" })
+        continue
+
     if parse_json.get("step") != "result":
         print("  Brain: ", parse_json.get("content"))
         continue
 
     print("Bot: ", parse_json.get("content"))
     break
+
+
+## CoT - Article (Github) HOMEWORK
+
+
+## Self-Consistency Prompting --> get the answers of the query from different models and at last by using different model/LLM get the most accurate/prob answer for the user
+
+
+## Persona - based Prompting --> feeding all the things about the person which you have to make the persona in system prompt.
+# These is completely based on the examples, CoT implementation and others
